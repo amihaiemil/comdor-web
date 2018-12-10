adbe4d1d113dbbf80b1862306e78bc1729a0faf6
amihaiemil <amihaiemil@gmail.com>
2018-12-10 20:20:21 +0200

    pom.xml and package renaming

--- src/main/java/co/comdor/web/ComdorWebApplication.java
+++ src/main/java/co/comdor/web/ComdorWebApplication.java
@@ -1,4 +1,4 @@
-package com.amihaiemil.web.comdorweb;
+package co.comdor.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/co/comdor/web/ComdorWebApplicationTests.java
+++ src/test/java/co/comdor/web/ComdorWebApplicationTests.java
@@ -1,4 +1,4 @@
-package com.amihaiemil.web.comdorweb;
+package co.comdor.web;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
