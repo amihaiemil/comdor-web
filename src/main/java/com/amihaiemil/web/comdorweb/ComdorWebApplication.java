21be627fe08fe0ea3516d03ce1b09960174c2939
amihaiemil <amihaiemil@gmail.com>
2018-12-10 20:02:33 +0200

    #1 maven + Spring Boot skeleton

---
+++ src/main/java/com/amihaiemil/web/comdorweb/ComdorWebApplication.java
@@ -0,0 +1,12 @@
+package com.amihaiemil.web.comdorweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComdorWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComdorWebApplication.class, args);
+	}
+}
